1785ac5a2907e4f42d28ce5a395547d04210eb8b
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-02-27 16:28:54 +0100

    Revert "switch from react to preact via build-time alias"
    This reverts commit b61b652f5ca4a517ddf301d3b5825ddeb478307a.

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -91,14 +91,8 @@ export default defineConfig((options) => [
     splitting: false,
     sourcemap: true,
     clean: !options.watch,
+    external: ["react", "react-dom"],
     esbuildPlugins: [scssModulesPlugin()],
-    esbuildOptions(options) {
-      options.alias = {
-        "react": "preact/compat",
-        "react-dom": "preact/compat",
-      };
-    },
-    noExternal: ["preact"],
     define: {
       __VERSION__: JSON.stringify(VERSION),
     },
